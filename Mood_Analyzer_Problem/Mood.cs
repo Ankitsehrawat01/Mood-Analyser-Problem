83130e5ab76fa4680e3efcc043aa883b17af918e
Ankitsehrawat01 <sehrawata328@gmail.com>
2022-09-03 03:14:34 +0530

    Analyse Happy or Sad Mood

---
+++ Mood_Analyzer_Problem/Mood.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mood_Analyzer_Problem
+{
+    public class Mood
+    {
+        public string MoodAnalyser(string Mood)
+        {
+            if (Mood.ToLower().Contains("sad"))
+            {
+                Console.WriteLine("Sad");
+                return "Sad";
+            }
+
+            else
+            {
+                Console.WriteLine("Happy");
+                return "Happy";
+            }
+        }
+    }
+}
---
+++ Mood_Analyzer_Problem/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Mood_Analyzer_Problem
+{
+    public class Program
+    {
+        public static string Happy_Mood = "I am in happy mood";
+        public static string Sad_Mood = "I am in sad mood";
+        public static void Main(string[] args)
+        {
+            Mood obj = new Mood();
+            obj.MoodAnalyser(Happy_Mood);
+            obj.MoodAnalyser(Sad_Mood);
+        }
+    }
+}
