71abd2e26e0059da743dfbbd068c7afe53cb8076
Ankitsehrawat01 <sehrawata328@gmail.com>
2022-09-03 03:54:13 +0530

    Refactor Code to take Mood message in constructor

---
+++ Mood_Analyzer_Problem/MoodAnalyse.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mood_Analyzer_Problem
+{
+    public class MoodAnalyse
+    {
+        public string Mood;
+        public MoodAnalyse(string Mood)
+        {
+            this.Mood = Mood;
+        }
+        public string MoodAnalyser()
+        {
+            if (Mood.ToLower().Contains("sad"))
+            {
+                Console.WriteLine("Sad");
+                return "Sad";
+            }
+
+            else
+            {
+                Console.WriteLine("Happy");
+                return "Happy";
+            }
+        }
+    }
+}
--- Mood_Analyzer_Problem/Program.cs
+++ Mood_Analyzer_Problem/Program.cs
@@ -6,9 +6,28 @@
         public static string Sad_Mood = "I am in sad mood";
         public static void Main(string[] args)
         {
-            Mood obj = new Mood();
-            obj.MoodAnalyser(Happy_Mood);
-            obj.MoodAnalyser(Sad_Mood);
+            bool check = true;
+            while(check)
+            {
+                Console.WriteLine("Welcome To Mood Analyser Problem");
+                Console.WriteLine("1) Mood Analyse\n" +
+                                  "2) Mood Analyse using Mood as Constructor\n");
+                int position = Convert.ToInt32(Console.ReadLine());
+                switch(position)
+                {
+                    case 1:
+                        Mood obj = new Mood();
+                        obj.MoodAnalyser(Happy_Mood);
+                        obj.MoodAnalyser(Sad_Mood);
+                        break;
+                    case 2:
+                        MoodAnalyse obj1 = new MoodAnalyse(Happy_Mood);
+                        obj1.MoodAnalyser();
+                        MoodAnalyse obj2 = new MoodAnalyse(Sad_Mood);
+                        obj2.MoodAnalyser();
+                        break;
+                }
+            }
         }
     }
 }
